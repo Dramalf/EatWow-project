ad73e320361b827bb405c88404cda905ea2a989f
Dramalf <176182986@qq.com>
2021-03-08 23:23:26 +0800

    '3-8'

---
+++ .history/src/components/StartBtn/index_20210308231704.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { Button } from 'antd-mobile';
+import { Axios } from '../../axios'
+import { connect } from 'react-redux'
+import { meal } from '../../redux/actions/setMeal'
+import qs from 'qs'
+import './index.css'
+const StartBtn = (props) => {
+    const [tags, settags] = useState([]);
+    const startChoose = () => {
+
+        const ajaxData = { username: 'mlf', userid: '916', tags }
+        console.log("发送", ajaxData)
+        Axios.post('http://127.0.0.1:5053/randommeal', qs.stringify(ajaxData))
+            .then(res => {
+                console.log("收到", res.data)
+                props.meal(res.data)
+            })
+    }
+
+    useEffect(() => {
+        settags(props.tags)
+    })
+
+    return (
+        <div >
+            <Button className='btn' type="primary" size="large" onClick={startChoose}>开始</Button>
+        </div>
+
+    );
+}
+
+export default connect(
+    state => state,
+    { meal }
+)(StartBtn)
